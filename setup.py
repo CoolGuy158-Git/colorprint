9d72a1507f4b108b5bc2bb0e9c8937ac74b1b863
CoolGuy158 <CoolGuy158-Proton@proton.me>
2025-12-20 14:52:26 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+###########################################################
+# Colorprint- Python module that lets you print colorfully#
+# Under MIT license CoolGuy158-Git                        #
+###########################################################
+from setuptools import setup, find_packages
+
+setup(
+    name="colorprint",
+    version="0.1",
+    packages=find_packages(),
+    description="Print colorful text in terminal easily",
+    author="CoolGuy158-Git",
+    license="MIT",
+)
