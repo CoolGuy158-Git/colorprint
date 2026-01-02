5db13be5eac64ece7bad7459028d5cdf7cc8ecc8
CoolGuy158 <CoolGuy158-Proton@proton.me>
2026-01-02 20:30:51 +1200

    Delete colorprint directory

--- colorprint/__init__.py
+++
@@ -1,7 +0,0 @@
-###########################################################
-# Colorprint- Python module that lets you print colorfully#
-# Under MIT license CoolGuy158-Git                        #
-###########################################################
-
-from .core import cprint, Colors
-
