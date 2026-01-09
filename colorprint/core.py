94f3e67ec181965706dc55e9f39d610589b1e46e
CoolGuy158 <CoolGuy158-Proton@proton.me>
2026-01-10 00:14:43 +1200

    Add files via upload

--- colorprint/core.py
+++ colorprint/core.py
@@ -1,7 +1,7 @@
 ###########################################################
 # Colorprint- Python module that lets you print colorfully#
 # With CSS-like chaining in terminal                      #
-# MIT license CoolGuy158-Git                               #
+# MIT license CoolGuy158-Git                              #
 ###########################################################
 
 class Colors:
@@ -13,6 +13,17 @@ class Colors:
     MAGENTA = "\033[95m"
     CYAN = "\033[96m"
     RESET = "\033[0m"
+    
+    # Background colors
+    BG_RED = "\033[41m"
+    BG_GREEN = "\033[42m"
+    BG_YELLOW = "\033[43m"
+    BG_BLUE = "\033[44m"
+    BG_MAGENTA = "\033[45m"
+    BG_CYAN = "\033[46m"
+    BG_WHITE = "\033[47m"
+    
+    # Styles
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
     ITALIC = "\033[3m"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py-colorprint-cg",
-    version="0.2.1",
+    version="0.3.3",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
